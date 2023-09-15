dab88c6339a3ad7dd4067ffcb7add0a7cba3c6d9
YuZhong-Chen <yuzhong1214@gmail.com>
2023-09-15 14:50:24 +0800

    Rewrite predict.py

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,8 @@
 
 pip install -r ~/Lab6/requirements.txt
 
+# Disable showwing (bash) in terminal
+conda config --set changeps1 False
+
 git clone git@github.com:YuZhong-Chen/.setup_env.git ~/.setup_env
 cd ~/.setup_env && ./install.sh
